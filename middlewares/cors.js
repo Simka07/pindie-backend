172d44ff56b28194e8b2d507e304330cba0e48a6
SimkaMari <masha@simonenko.ru>
2024-05-22 22:01:30 +0300

    добавила домен

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
     'https://students-projects.ru',
     'http://localhost:3000',
     'http://localhost:3001',
-    
+    "https://front-pindie-web.nomoredomainswork.ru/api",
   ];
   
   // Функция, которая принимает объекты req (информация о запросе),
